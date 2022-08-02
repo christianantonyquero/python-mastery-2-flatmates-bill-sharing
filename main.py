a43dc3c9155f9fb8c5d03899e0e84d86bba979f4
Christian Antony Quero <christianantonyquero@gmail.com>
2022-08-02 21:05:41 +0800

    Created a User Interface (CLI).

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import webbrowser
+
 from fpdf import FPDF
 
 
@@ -82,12 +83,21 @@ class PdfReport:
         # webbrowser.open('file://' + os.path.realpath(self.filename))
 
 
-the_bill = Bill(amount=120, period="April 2021")
-john = Flatmate(name="John", days_in_house=20)
-marry = Flatmate(name="Marry", days_in_house=25)
-chris = Flatmate(name="Chris", days_in_house=15)
+# Enter bill information
+bill_amount = float(input("Hey user, enter the bill amount: "))
+period = input("What is the billing period? E.g. December 2022: ")
+the_bill = Bill(amount=bill_amount, period=period)
 
-flatmate_list = [john, marry, chris]
+# Enter flatmate details
+number_of_flat_mates = int(input("Number of flatmates?: "))
+flatmate_list = []
 
-pdf_report = PdfReport(filename="Report.pdf")
+for number in range(1, number_of_flat_mates + 1):
+    name = input("Enter flatmate " + str(number) + " name: ")
+    days_in_house = int(input("Enter how many days" + name + "stayed in the house during the bill period: "))
+
+    flatmate = Flatmate(name, days_in_house)
+    flatmate_list.append(flatmate)
+
+pdf_report = PdfReport(filename=f"{the_bill.period}_Report.pdf")
 pdf_report.generate(flatmate_list, the_bill)
