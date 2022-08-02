44d970b3e8c5ddc4a99cfcec28541d16233ce910
Christian Antony Quero <christianantonyquero@gmail.com>
2022-08-02 20:08:58 +0800

    Modify algorithm and comments

--- main.py
+++ main.py
@@ -23,10 +23,15 @@ class Flatmate:
         self.days_in_house = days_in_house
 
     def pays(self, bill, flatmates):
+        # Compute total days all the flatmates stayed in the house.
         flatmates_total_days_in_house = 0
-        for flatmate in flatmates:
-            flatmates_total_days_in_house += flatmate.days_in_house
-        weight = self.days_in_house / (self.days_in_house + flatmates_total_days_in_house)
+        for flatmate_instance in flatmates:
+            flatmates_total_days_in_house += flatmate_instance.days_in_house
+
+        # Compute weight for the  flatmate
+        weight = self.days_in_house / flatmates_total_days_in_house
+
+        # Total amount flatmate will pay
         to_pay = bill.amount * weight
         return to_pay
 
